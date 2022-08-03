980835ed2f61b03b2c6c8f9d6ae8a4127097beb7
johnatan-ferre <johnatanferre@gmail.com>
2022-08-02 23:51:36 -0300

    se completa codigo

--- Contenedor.js
+++ Contenedor.js
@@ -2,45 +2,58 @@ const { promises: fs } = require('fs');
 
 class Contenedor {
     constructor(ruta) {
-        this.ruta = ruta;
+        this.ruta = ruta
     }
 
 
     async save(object) {
-        const items = await fs.readFile(this.ruta, 'utf-8');
-        if (items) {
-            JSON.parse(items);
-            let itemId = items.id;
-            if (itemId.length-1 >= 0) {
-                fs.appendFile(this.ruta, JSON.stringify(items, null, 2));
-                itemId++;
-            } else {
-                fs.writeFile(this.ruta, JSON.stringify(items, null, 2))
-            }
+        const items = await this.getAll()
+        object.id = items[items.length - 1].id + 1
+        items.push(object)
+        try {
+            console.log(`Se va a guardar el siguiente item: ${JSON.stringify(object)}`)
+            await fs.writeFile(this.ruta, JSON.stringify(items, null, 2))
+            console.log('Guardado ok.')
+        } catch (error) {
+            console.log('Error de escritura')
+            console.log(error)
         }
     }
 
     async getById(id) {
         const items = await this.getAll();
-        const itemById = items.find(item => item.id == id);
+        const itemById = items.find(item => item.id == id)
+        return itemById
     }
 
     async getAll() {
         try {
-            const items = await fs.readFile(this.ruta, 'utf-8');
+            const items = await fs.readFile(this.ruta, 'utf-8')
             return JSON.parse(items);
         } catch (error) {
-            return [];            
+            console.log('Error de lectura.')
+            console.log(error)
+            return []          
         }
     }
 
     async deleteById(id) {
-        const items = await this.getAll();
-        const deleteItemById = items.remove(item => item.id == id)
+        const items = await this.getAll()
+        const deleteItemById = items.find(item => item.id === id)
+        items.splice(deleteItemById, 1)
+        await fs.writeFile(this.ruta, JSON.stringify(items))
+        console.log(`Se elimina el elemento ${deleteItemById}`)
     }
 
     async deleteAll() {
-        const items = await fs.unlink(this.ruta);
+        try {
+            console.log('Todos los elementos van a ser borrados..')
+            await fs.unlink(this.ruta)
+            console.log('Elementos eliminados con éxito.')
+        } catch (error) {
+            console.error('Error de escritura.')
+            console.error(error)
+        }
     }
 }
 
--- test.js
+++ test.js
@@ -1,29 +1,38 @@
 const Contenedor = require('./Contenedor');
 
 async function main() {
-    const item = new Contenedor('./items.txt');
+    const items = new Contenedor('./items.txt');
+
+    console.log('Cargamos el primer item');
+    let addedItem1 = await items.save(({title: 'Escuadra',price: 123.45, thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png'}))
+    console.log(addedItem1);
+
+    console.log('Cargamos el segundo item');
+    let addedItem2 = await items.save(({title: 'Calculadora', price: 234.56, thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/calculator-math-tool-school-256.png'}))
+    console.log(addedItem2);
+
+    console.log('Cargamos el tercer item');
+    let addedItem3 = await items.save(({title: 'Globo Terráqueo', price: 345.67, thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/globe-earth-geograhy-planet-school-256.png'}))
+    console.log(addedItem3);
 
 
-    console.log('Item agregado');
-    let addedItem = await item.save(item);
-    console.log(addedItem);
 
     console.log('Mostrando todos los items');
-    let allItems = await item.getAll();
+    let allItems = await items.getAll();
     console.log(allItems);
 
     const idToSearch = 1;
     console.log(`Mostrando item con id ${idToSearch}`);
-    let itemById = await item.getById(idToSearch);
+    let itemById = await items.getById(idToSearch);
     console.log(itemById);
 
     const idToDelete = 1;
     console.log(`Borrar item con id ${idToDelete}`);
-    let deleteItemById = await item.deleteById(idToDelete);
+    let deleteItemById = await items.deleteById(idToDelete);
     console.log(deleteItemById);
 
-    console.log('Todo borrado');
-    let deleteAllItems = await item.deleteAll();
+    console.log('Borrar todo');
+    let deleteAllItems = await items.deleteAll();
     console.log(deleteAllItems);
 }
 
